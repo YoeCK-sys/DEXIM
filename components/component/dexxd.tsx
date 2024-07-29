f774fee9c126950d6e0c05a86b32356ab4e4121d
YoeCK-sys <114443942+YoeCK-sys@users.noreply.github.com>
2024-07-29 12:59:28 -0300

    xd
    misds

--- components/component/dexxd.tsx
+++ components/component/dexxd.tsx
@@ -77,7 +77,7 @@ export function XDXD() {
       <main className="flex-1 overflow-auto p-4">
         <div className="space-y-4">
           {accordionData.map((item, index) => (
-            <Card key={index} className="bg-gray-800 bg-opacity-70 text-white backdrop-blur-lg">
+            <Card key={index} className="bg-gray-800 bg-opacity-0 text-white backdrop-blur-lg border-none">
               <CardHeader>
                 <Accordion
                   i={index}
